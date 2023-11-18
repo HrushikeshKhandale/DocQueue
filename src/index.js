3ad82a0576e01d3cfbabd624a3e0044ad7624a3b
Hrushikesh Khandale <khandalehrushikesh@gmail.com>
2023-11-18 23:55:00 +0530

    error fixed

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {AuthProvider} from "./components/context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider >
       <App />
-    </AuthProvider >
   </React.StrictMode>
 );
 
